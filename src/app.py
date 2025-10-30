14cf38e886effa8044356e3764fdcd5f5bddf724
Camille Kerserho <camille.kerserho@epitech.eu>
2025-10-30 16:50:17 +0100

    Delete Redis

--- src/app.py
+++ src/app.py
@@ -5,4 +5,4 @@ from src.handlers import health, storage
 app = FastAPI()
 
 app.include_router(health.router, prefix="")
-app.include_router(storage.router, prefix="/storage")  # pylint: disable=no-member
+app.include_router(storage.router, prefix="/storage")
