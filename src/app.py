0f2b8cd741ddd3cbf15de530da72a9c29396e9ba
Camille Kerserho <camille.kerserho@epitech.eu>
2025-10-30 11:24:19 +0100

    Add healthCheck endpoint

--- src/app.py
+++ src/app.py
@@ -4,5 +4,5 @@ from src.handlers import health, storage
 
 app = FastAPI()
 
-app.include_router(health.router, prefix="/health")
+app.include_router(health.router, prefix="")
 app.include_router(storage.router, prefix="/storage")  # pylint: disable=no-member
