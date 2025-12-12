9cec48ad18288f4e2dfc18f81b4de014a9795728
vicous <debian@DESKTOP-ELI6983.>
2025-12-12 16:15:31 +0100

    tst

--- terraform/get.sh
+++ terraform/get.sh
@@ -1,8 +1,11 @@
 # Get the SAS token directly from Terraform output
-TOKEN="a trouver dans la ci"
+TOKEN="a trouver dans la ci " # exemple : TOKEN="sv=2018-11-09&sr=c&st=2025-12-12T03:14:25Z..."
 
 # Base URL
 URL="https://blobpourcamille.blob.core.windows.net/tf-container"
 
 # Download blob named "example.txt"
 curl -o downloaded_example.txt "$URL/example.txt?$TOKEN"
+
+
+# le ficher arrivera sous la forme downloaded_exemple.tkt une fois recupéré
